cfe8f3f7d1a9534a96d9f83b8c4578b52a053745
Ritika Soni <soniritika172006@gmail.com>
2026-02-16 21:33:27 +0530

    frontend

--- frontend/src/lib/toast.js
+++ frontend/src/lib/toast.js
@@ -1,11 +1,13 @@
 import { toast } from 'sonner';
+import React from 'react';
+import ErrorIcon from './ErrorIcon';
 
 export function toastSuccess(message) {
   return toast.success(message);
 }
 
 export function toastError(message) {
-  return toast.error(message);
+  return toast.error(message, { icon: React.createElement(ErrorIcon) });
 }
 
 export function toastInfo(message) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,13 +13,6 @@ export default defineConfig({
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
-              // React and related only in react-vendor
-              if (/node_modules[\\/](react|react-dom|react-router|react-router-dom)/.test(id)) return 'react-vendor';
-              // pdfjs-dist only in pdfjs-vendor
-              if (/node_modules[\\/]pdfjs-dist/.test(id)) return 'pdfjs-vendor';
-              // Lodash and similar only in vendor
-              if (/node_modules[\\/](lodash|lodash-es|lodash.debounce|lodash.merge|underscore)/.test(id)) return 'vendor';
-              // All other node_modules in vendor
               return 'vendor';
             }
           },
