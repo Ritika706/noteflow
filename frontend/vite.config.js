4fa9b7a74cf22b9ed087ef666c78e073bab91d46
Ritika Soni <soniritika172006@gmail.com>
2026-02-16 21:10:25 +0530

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,10 +13,13 @@ export default defineConfig({
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
-              // Prioritize react-vendor, then pdfjs-vendor, then vendor
-              if (/node_modules[\\/]react/.test(id)) return 'react-vendor';
+              // React and related only in react-vendor
+              if (/node_modules[\\/](react|react-dom|react-router|react-router-dom)/.test(id)) return 'react-vendor';
+              // pdfjs-dist only in pdfjs-vendor
               if (/node_modules[\\/]pdfjs-dist/.test(id)) return 'pdfjs-vendor';
-              if (/node_modules[\\/](lodash|underscore)/.test(id)) return 'vendor';
+              // Lodash and similar only in vendor
+              if (/node_modules[\\/](lodash|lodash-es|lodash.debounce|lodash.merge|underscore)/.test(id)) return 'vendor';
+              // All other node_modules in vendor
               return 'vendor';
             }
           },
